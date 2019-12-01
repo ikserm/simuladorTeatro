d8eaab4cd3d488c1e944c405b5a341ffef9680eb
Ikser Marquez <iksermd@gmail.com>
2019-11-30 22:29:27 -0600

    C2

---
+++ historial.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+</head>
+<body>
+
+	
+
+<div class="container">
+	<br>
+	<h2>Historial de simulaciones</h2>
+	<br>
+	<button type="button"  class="btn btn-outline-secondary"><a href="Index.html">Regresar</a></button>
+	<br><br>
+	<div class="row align-items-center">
+		<table class="table">
+			<th>
+				<td>
+					Simulacion
+				</td>
+				<td>
+					Atributo1
+				</td>
+				<td>
+					Atributo2
+				</td>
+				<td>
+					Atributo3
+				</td>
+				<td>
+					Atributo4
+				</td>
+			</th>
+			<tr>
+				<td>
+					.
+				</td>
+				<td>
+					.
+				</td>
+				<td>
+					.
+				</td>
+				<td>
+					.
+				</td>
+				<td>
+					.
+				</td>
+			</tr>
+		</table>		
+	</div>	
+</div>
+	<script src="js/bootstrap.js" ></script>
+	<script src="js/jquery-3.4.1.slim.min.js" ></script>
+	<script src="js/popper.min.js" ></script>
+</body>
+</html>
--- teatro.php
+++ teatro.php
@@ -7,7 +7,244 @@
 </head>
 <body>
 
+	
 
+<div class="container">
+	<br>
+	<h2>Butacas</h2>
+	<br>
+	<button type="button"  class="btn btn-outline-secondary"><a href="Index.html">Regresar</a></button>
+	<br><br>
+	<div class="row align-items-center">
+		<table >
+			<tr>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>						
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>						
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>						
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+
+			<tr>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>		
+			<tr>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+				<td>
+					<div class=" alert alert-danger" style="width: 4rem; height: 4rem;">		  </div>
+				</td>
+			</tr>
+		</table>		
+	</div>	
+</div>
 	<script src="js/bootstrap.js" ></script>
 	<script src="js/jquery-3.4.1.slim.min.js" ></script>
 	<script src="js/popper.min.js" ></script>
