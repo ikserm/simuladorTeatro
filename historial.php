46b41586c449cb401ce111aff96ee8822ff7bd7d
jelsyn <jelsync@gmail.com>
2019-11-30 23:27:30 -0600

    detalles

--- historial.php
+++ historial.php
@@ -1,78 +1,92 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title></title>
 	<link rel="stylesheet" href="css/bootstrap.css">
 
 </head>
+
 <body>
 
-	
 
-<div class="container">
-	<br>
-	<h2>Historial de simulaciones</h2>
-	<br>
-	<button type="button"  class="btn btn-outline-secondary"><a href="Index.html">Regresar</a></button>
-	<br><br>
-	<div class="">
-		<table class="table">
-			<tr>
-				<th>
-					Atributo
-				</th>
-				<th>
-					Atributo
-				</th>
-				<th>
-					Atributo
-				</th>
-				<th>
-					Atributo
-				</th>
-				<th>
-					Atributo
-				</th>
-			</tr>
-			<tr>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-				<td>
-					Atributo
-				</td>
-			</tr>
-		</table>		
-	</div>	
-</div>
-	<script src="js/bootstrap.js" ></script>
-	<script src="js/jquery-3.4.1.slim.min.js" ></script>
-	<script src="js/popper.min.js" ></script>
+
+	<div class="container">
+		<br>
+		<h2>Historial de simulaciones</h2>
+		<br>
+		<button type="button" class="btn btn-outline-secondary"><a href="Index.html">Regresar</a></button>
+		<br><br>
+		<div class="">
+			<table class="table">
+				<tr>
+					<th>
+					Cantidad de filas
+					</th>
+					<th>
+					Cantidad de sillas por filas
+					</th>
+					<th>
+					Número de grupos que ingresaron
+					</th>
+					<th>
+					Tamaños de los grupos [Rango]
+					</th>
+					<th>
+					Cantidad de sillas desperdiciadas
+					</th>
+					<th>
+					Cantidad de grupos rechazados
+					</th>
+				</tr>
+				<tr>
+					<td>
+						101
+					</td>
+					<td>
+						10
+					</td>
+					<td>
+						5
+					</td>
+					<td>
+						1-10
+					</td>
+					<td>
+						0
+					</td>
+					<td>
+						2
+					</td>
+					
+				</tr>
+				<tr>
+					<td>
+						50
+					</td>
+					<td>
+						8
+					</td>
+					<td>
+						5
+					</td>
+					<td>
+						1-5
+					</td>
+					<td>
+						2
+					</td>
+					<td>
+						0
+					</td>
+					
+				</tr>
+			</table>
+		</div>
+	</div>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/jquery-3.4.1.slim.min.js"></script>
+	<script src="js/popper.min.js"></script>
 </body>
+
 </html>
