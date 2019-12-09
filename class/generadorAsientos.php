438c8641c514aa2e2e590905c7f86d48f6f4f2f5
ikserm <iksermd@gmail.com>
2019-12-08 22:59:26 -0600

    ++

--- class/generadorAsientos.php
+++ class/generadorAsientos.php
@@ -1,16 +1,19 @@
 <?php
 $fila=$_REQUEST["fila"];
 $columna=$_REQUEST["columna"];
+$min_grupo=$_REQUEST["min_grupo"];
+$max_grupo=$_REQUEST["max_grupo"];
+$grupos_rechazados=$_REQUEST["grupos_rechazados"];
+
 $num=$_REQUEST["i"];
 
 echo "<table>";
-echo "<h3>".$num."</h3>";
 for ($i=0; $i < $fila; $i++) { 
 	echo "<tr>";
 	for ($j=0; $j < $columna; $j++) { 
-		echo "<td>";
-		echo $i.$j;
-		echo "</td>";
+		echo '	<td>
+					<div class=" alert alert-success" style="width: 4rem; height: 4rem;">		  </div>
+				</td>';
 	}
 	echo "</tr>";
 }
